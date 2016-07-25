bd33bd3a945b5a2f94e97b9698dade3859d22d45
vikrekram <79538830334@yandex.ru>
2016-07-25 12:39:31 +1000

    Создан основной файл управления приложением

---
+++ main.js
@@ -0,0 +1,37 @@
+const electron = require('electron');
+// Модуль контроля жизненного цикла приложения
+const {app} = electorn;
+// Модуль создания нативного окна браузера
+const {BrowserWindow} = electron;
+
+let win;
+
+function createWindow() {
+  // Создаем окно браузера
+  win = new BrowserWindow({width: 800, height: 600});
+
+  // и загружаем index.html приложения
+  win.loadURL(`file://${__dirname}/index.html`);
+
+  // Открываем DevTools
+  win.webContents.openDevTools();
+
+  // Сообщаем когда окно закрыто
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
